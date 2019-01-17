9496dd293d48a4442669c7621fd7c95797fc7f25
minblock <38344452+minblock@users.noreply.github.com>
2019-01-17 17:27:34 -0500

    Rebased ports

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -280,8 +280,8 @@ def run_tests(test_list, src_dir, build_dir, exeext, tmpdir, jobs=1, enable_cove
         print("%sWARNING!%s There is a cache directory here: %s. If tests fail unexpectedly, try deleting the cache directory." % (BOLD[1], BOLD[0], cache_dir))
 
     #Set env vars
-    if "PICSCOIND" not in os.environ:
-        os.environ["PICSCOIND"] = build_dir + '/src/OGd' + exeext
+    if "OGD" not in os.environ:
+        os.environ["OGD"] = build_dir + '/src/OGd' + exeext
         os.environ["PICSCOINCLI"] = build_dir + '/src/OG-cli' + exeext
 
     tests_dir = src_dir + '/test/functional/'
